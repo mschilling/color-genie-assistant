e2f70a9f296a780fe27f207d68cfbc5e829e8e6b
Michael Schilling <mschilling@arkid.nl>
2018-07-27 09:23:36 +0200

    Implement hex-to-color-name + add tests

---
+++ functions/src/tests/hex-to-color-test.ts
@@ -0,0 +1,8 @@
+const colors = require('hex-to-color-name');
+
+
+console.log(`Color #800080 is ${colors('#800080')}`);
+console.log(`Color #800000 is ${colors('#800000')}`);
+// console.log(`Color #800080 is ${colors('#800080')}`);
+// console.log(`Color #800080 is ${colors('#800080')}`);
+
